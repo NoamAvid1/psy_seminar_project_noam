882a9087f629960f48c5a6f20b42f594f6b23a56
t-noamavidor <t-noamavidor@microsoft.com>
2023-10-04 22:54:34 +0300

    Added posthoc

--- anova/3_way_anova.R
+++ anova/3_way_anova.R
@@ -13,7 +13,7 @@ if (!requireNamespace("ez", quietly = TRUE)) {
 }
 library(ez)
 
-df <- read.csv("C:/Users/t-noamavidor/Documents/personal/University/Face_recognition_seminar/psy_seminar_project_noam/seminar_dists_results/experiment_1_1/batches_aucs/all.csv")
+df <- read.csv("C:/Users/t-noamavidor/Documents/personal/University/Face_recognition_seminar/psy_seminar_project_noam/seminar_dists_results/experiment_1_2/batches_aucs/all.csv")
 
 
 # summary statistics:
@@ -21,7 +21,7 @@ stat_df <- df %>%
   group_by(dcnn_domain, img_domain, orientation) %>%
   get_summary_stats(auc, type = "mean_sd")
 
-# write.csv(stat_df, "C:/Users/t-noamavidor/Documents/personal/University/Face_recognition_seminar/psy_seminar_project_noam/seminar_dists_results/experiment_1_1/batches_aucs/stats.csv")
+write.csv(stat_df, "C:/Users/t-noamavidor/Documents/personal/University/Face_recognition_seminar/psy_seminar_project_noam/seminar_dists_results/experiment_1_2/batches_aucs/stats.csv")
 
 
 anova_result <- ezANOVA(data = df, 
@@ -40,11 +40,16 @@ res.aov <- anova_test(
   data = df, dv = auc, wid = id,
   between = dcnn_domain, within = c(img_domain, orientation)
 )
-get_anova_table(res.aov)
+anova_table <- get_anova_table(res.aov)
+write.csv(anova_table, "C:/Users/t-noamavidor/Documents/personal/University/Face_recognition_seminar/psy_seminar_project_noam/seminar_dists_results/experiment_1_2/batches_aucs/anova_table.csv")
 
 
-# Perform Tukey's post-hoc test
-posthoc <- TukeyHSD(anova_result[[1]])
 
-# View the results
-print(posthoc)
+# PostHoc
+print("Post-hoc:-------------")
+# Two-way ANOVA at each orientation group level
+two.way <- df %>%
+  group_by(img_domain) %>%
+  anova_test(dv = auc, wid = id, within = c(orientation))
+print(two.way)  
+write.csv(two.way, "C:/Users/t-noamavidor/Documents/personal/University/Face_recognition_seminar/psy_seminar_project_noam/seminar_dists_results/experiment_1_2/batches_aucs/post_hoc_inversion.csv")
